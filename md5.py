6a75e05f2b3c25718fcc4ced0b4aa2eca77a03e6
Lukasz <lukaszs@gmail.com>
2018-12-04 11:31:04 +0100

    Update md5.py

--- md5.py
+++ md5.py
@@ -1,13 +1,15 @@
-import glob
 import hashlib
 import os
 
 
-Path = "."
+Path = "C://Users//m121374//PycharmProjects//gogole//venv"
 filelist = os.listdir(Path)
 print(filelist)
 
-with open('iexplore.exe','rb') as getmd5:
-    data = getmd5.read()
-    gethash= hashlib.md5(data).hexdigest()
-    print(gethash)
+for element in filelist:
+    fullpath = Path+'//' +element
+    if not os.path.isdir(fullpath):
+        with open(fullpath,'rb') as getmd5:
+            data = getmd5.read()
+            gethash= hashlib.md5(data).hexdigest()
+            print(gethash)
